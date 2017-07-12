6936532dbab8844c100095d5efeb248f27a5dc79
David Monagle <david@monagle.com.au>
2017-07-12 10:28:46 +1000

    Changed to module based structure

--- Package.swift
+++ Package.swift
@@ -2,6 +2,10 @@ import PackageDescription
 
 let package = Package(
     name: "VaporGraph",
+    targets: [
+        Target(name: "VaporGraph", dependencies: ["GraphVaporExtensions"]),
+        Target(name: "GraphVaporExtensions"),
+    ],
     dependencies: [
         .Package(url: "https://github.com/vapor/fluent-provider.git", majorVersion: 1)
     ]
